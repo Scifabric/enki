328a59e4c57c4b27c77c01c59700958a75071038
Daniel Lombraña González <teleyinex@gmail.com>
2013-12-16 11:53:15 +0100

    Requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='enki',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=['pybossa-client', 'pandas'],
+    # metadata for upload to PyPI
+    author='Daniel Lombraña González',
+    author_email='teleyinex@gmail.com',
+    description='A Python library to analyze PyBossa application results',
+    long_description='''PyBossa is a crowdsourcing framework. This tiny library that allows you to analyze the results of a PyBossa aplication.''',
+    license='AGPLv3',
+    url='https://github.com/pybossa/enki',
+    download_url='https://github.com/PyBossa/enki/zipball/master',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: AGPLv3 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points=''''''
+)
