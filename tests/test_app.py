3d2a5f31be06774f7d8c868fddc59510e3c6ca62
Daniel Lombraña González <teleyinex@gmail.com>
2013-12-16 11:52:45 +0100

    Unit Tests for Enki

---
+++ tests/test_app.py
@@ -0,0 +1,158 @@
+# -*- coding: utf8 -*-
+# This file is part of PyBossa.
+#
+# Copyright (C) 2013 SF Isle of Man Limited
+#
+# PyBossa is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBossa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBossa.  If not, see <http://www.gnu.org/licenses/>.
+"""Package to test Enki package."""
+import enki
+from enki.exceptions import AppNotFound, AppError, \
+    AppWithoutTasks, AppWithoutTaskRuns
+from mock import patch
+from base import TestEnki
+from nose.tools import raises
+
+
+class Test(TestEnki):
+    @raises(AppNotFound)
+    @patch('pbclient.requests.get')
+    def test_00_get_app_not_found(self, Mock):
+        """Test app not found works."""
+        # App does not exist returns an empty list
+        Mock.return_value = self.create_fake_request(data=[], status=200)
+        enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                  app_short_name='non-exists')
+
+    @patch('pbclient.requests.get')
+    def test_01_get_app_found(self, Mock):
+        """Test app found works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        assert e.app.id == self.app['id'], e.app
+        assert e.app.short_name == self.app['short_name'], e.app
+
+    @patch('pbclient.requests.get')
+    def test_explode_info_without_info_dict(self, Mock):
+        """Test explode_info method works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        Mock.side_effect = [self.create_fake_request([self.task], 200),
+                            self.create_fake_request([], 200)]
+        e.get_tasks()
+        result = e.explode_info(e.tasks[0])
+        err_msg = "This item should not be exploded"
+        assert result.keys() == self.task.keys(), err_msg
+
+    @patch('pbclient.requests.get')
+    def test_explode_info_with_info_dict(self, Mock):
+        """Test explode_info method works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        Mock.side_effect = [self.create_fake_request([self.task2], 200),
+                            self.create_fake_request([], 200)]
+        e.get_tasks()
+        result = e.explode_info(e.tasks[0])
+        err_msg = "This item should be exploded"
+        assert 'key' in result.keys(), err_msg
+
+    @raises(AppError)
+    @patch('pbclient.requests.get')
+    def test_get_tasks_app_error(self, Mock):
+        """Test get_tasks without app works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        # Make it fail
+        e.app = None
+        Mock.side_effect = [self.create_fake_request([], 200)]
+        e.get_tasks()
+
+    @raises(AppWithoutTasks)
+    @patch('pbclient.requests.get')
+    def test_get_tasks_empty(self, Mock):
+        """Test get_tasks without tasks works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        Mock.side_effect = [self.create_fake_request([], 200)]
+        e.get_tasks()
+
+    @patch('pbclient.requests.get')
+    def test_get_tasks(self, Mock):
+        """Test get_tasks with tasks works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        Mock.side_effect = [self.create_fake_request([self.task], 200),
+                            self.create_fake_request([], 200)]
+        e.get_tasks()
+        desc = e.tasks_df['info'].describe()
+        err_msg = "Pandas describe is wrong"
+        assert e.tasks_df['id'].count() == 1, err_msg
+        assert desc['count'] == 1, err_msg
+        assert desc['unique'] == 1, err_msg
+        assert desc['top'] == self.task['info'], err_msg
+        assert desc['freq'] == 1, err_msg
+
+    @raises(AppError)
+    @patch('pbclient.requests.get')
+    def test_get_task_runs_app_error(self, Mock):
+        """Test get_task_runs without task runs works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        Mock.side_effect = [self.create_fake_request([self.task], 200),
+                            self.create_fake_request([], 200)]
+        e.get_tasks()
+        # Make it fail
+        e.app = None
+        Mock.side_effect = [self.create_fake_request([], 200)]
+        e.get_task_runs()
+
+    @raises(AppWithoutTaskRuns)
+    @patch('pbclient.requests.get')
+    def test_get_task_runs_empty(self, Mock):
+        """Test get_task_runs without task runs works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        Mock.side_effect = [self.create_fake_request([self.task], 200),
+                            self.create_fake_request([], 200)]
+        e.get_tasks()
+
+        Mock.side_effect = [self.create_fake_request([], 200)]
+        e.get_task_runs()
+
+    @patch('pbclient.requests.get')
+    def test_get_task_runs(self, Mock):
+        """Test get_task_runs with task_runs works."""
+        Mock.return_value = self.create_fake_request([self.app], 200)
+        e = enki.Enki(api_key='key', endpoint='http://localhost:5000',
+                      app_short_name=self.app['short_name'])
+        Mock.side_effect = [self.create_fake_request([self.task], 200),
+                            self.create_fake_request([], 200)]
+        e.get_tasks()
+        Mock.side_effect = [self.create_fake_request([self.taskrun], 200),
+                            self.create_fake_request([], 200)]
+        e.get_task_runs()
+
+        desc = e.task_runs_df[e.tasks[0].id]['info'].describe()
+        err_msg = "Pandas describe is wrong"
+        assert desc['count'] == 1, err_msg
+        assert desc['unique'] == 1, err_msg
+        assert desc['top'] == self.task['info'], err_msg
+        assert desc['freq'] == 1, err_msg
