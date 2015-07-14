fc56c8caa4217af4b19ae4af25ff1a04bbc93136
alejandrodob <alejandrodob@gmail.com>
2015-07-14 17:12:02 +0200

    Update pybossa-client to v1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='enki',
     version='0.0.5',
     packages=find_packages(),
-    install_requires=['pybossa-client>=1.0.2, <1.0.3', 'pandas'],
+    install_requires=['pybossa-client>=1.0.3, <1.0.4', 'pandas'],
     # metadata for upload to PyPI
     author='Daniel Lombraña González',
     author_email='teleyinex@gmail.com',
