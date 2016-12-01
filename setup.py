a1d76a5877b3e089d0b01e6e809aa5c1b19f68db
Daniel Lombraña González <teleyinex@gmail.com>
2016-12-01 13:03:16 +0100

    Update version and docs.

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='enki',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     install_requires=['pybossa-client>=1.1.1, <1.1.2', 'pandas'],
     # metadata for upload to PyPI
     author='SciFabric LTD',
     author_email='info@scifabric.com',
-    description='A Python library to analyze PyBossa application results',
-    long_description='''PyBossa is a crowdsourcing framework. This tiny library that allows you to analyze the results of a PyBossa aplication.''',
+    description='A Python library to analyze PYBOSSA application results',
+    long_description='''PyBossa is a crowdsourcing framework. This tiny library that allows you to analyze the results of a PYBOSSA aplication.''',
     license='AGPLv3',
-    url='https://github.com/pybossa/enki',
-    download_url='https://github.com/PyBossa/enki/zipball/master',
+    url='https://github.com/Scifabric/enki',
+    download_url='https://github.com/Scifabric/enki/zipball/master',
     include_package_data=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
